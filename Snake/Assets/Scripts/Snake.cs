6694dab3a863b0682550cd30281e3c26744916c8
burakTuran00 <burakturan7410@gmail.com>
2022-12-01 23:16:25 +0300

    Last Attemt

---
+++ Snake/Assets/Scripts/Snake.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2 _direction = Vector2.right;
+    private List<Transform> _segments;
+    public Transform segmentPrefab;
+    private void Start()
+    {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _direction = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            _direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            _direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            _direction = Vector2.right;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = _segments.Count -1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+        this.transform.position = new Vector3(Mathf.Round(this.transform.position.x) + _direction.x, Mathf.Round(this.transform.position.y) + _direction.y, 0f);
+
+    }
+    public void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+        _segments.Add(segment);
+    }
+    private void ResetState()
+    {
+        for (int i = 1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+        _segments.Clear();
+        _segments.Add(this.transform);
+        this.transform.position = Vector2.zero;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Food"))
+        {
+            Grow();
+        }
+        else if (collision.CompareTag("Obstacle"))
+        {
+            ResetState();
+        }
+    }
+
+    
+}
+
