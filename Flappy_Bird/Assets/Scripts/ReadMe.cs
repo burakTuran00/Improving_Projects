be5a3b2844ce7d620295ba9c37ba95b6d1f88c5b
burakTuran00 <burakturan7410@gmail.com>
2022-12-01 12:33:17 +0300

    Reading

--- Flappy_Bird/Assets/Scripts/ReadMe.cs
+++
@@ -1,4 +0,0 @@
-/* Hi There,
- I'made a Flappy Bird. And I learnt too much in this project. Sometimes I looked where I made mistakes. So I fixed it.
-Burak Turan
- */
---
+++ Flappy_Bird/Assets/Scripts/ReadMe_1.cs
@@ -0,0 +1,3 @@
+// Hi There,
+// I learnt too much knowdlege here. Sometimes I looked where I made mistakes.
+// Burak Turan
