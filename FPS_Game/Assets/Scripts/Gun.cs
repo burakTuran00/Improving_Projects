ac69af5719b22868b7c0e7b4e61cc391710c2feb
burakTuran00 <burakturan7410@gmail.com>
2023-01-16 16:41:26 +0300

    FPS_Game

--- FPS_Game/Assets/Scripts/Gun.cs
+++ FPS_Game/Assets/Scripts/Gun.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
@@ -7,28 +9,80 @@ public class Gun : MonoBehaviour
     public float range = 100f;
 
     public Camera cross;
-   // public ParticleSystem effect;
+
+    // public ParticleSystem effect;
+    public int maxAmmount = 30;
+
+    private int currentAmount;
+
+    public float reloadTime = 1f;
+
+    private bool isReloading = false;
+
+    public Animator animator;
+
+    public Text ammount_Text;
+
+    private void Awake()
+    {
+        currentAmount = maxAmmount;
+        ammount_Text.text = "x" + currentAmount.ToString();
+    }
 
     private void Update()
     {
+        if (isReloading)
+        {
+            return;
+        }
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
+        }
+        if (currentAmount <= 0f)
+        {
+            StartCoroutine(Releod());
+            return;
         }
     }
 
     public void Shoot()
     {
-       // effect.Play();
+        // effect.Play();
+        currentAmount--;
+        ammount_Text.text = "x" + currentAmount.ToString();
         RaycastHit hit;
-        if (Physics.Raycast(cross.transform.position, cross.transform.forward, out hit, range))
+        if (
+            Physics
+                .Raycast(cross.transform.position,
+                cross.transform.forward,
+                out hit,
+                range)
+        )
         {
             Debug.Log(hit.transform.name.ToString());
             Target target = hit.transform.GetComponent<Target>();
             if (target != null)
             {
-                target.TakeDamage(damage);
+                target.TakeDamage (damage);
             }
         }
+    }
+
+    IEnumerator Releod()
+    {
+        isReloading = true;
+        Debug.Log("Reloding");
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+
+        animator.SetBool("Reloading", false);
+
+        yield return new WaitForSeconds(.25f);
+
+        currentAmount = maxAmmount;
+        isReloading = false;
+        ammount_Text.text = "x" + currentAmount.ToString();
     }
 }
--- FPS_Game/Assets/Scripts/MouseLook.cs
+++ FPS_Game/Assets/Scripts/MouseLook.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 150f;
 
     public Transform playerBody;
 
