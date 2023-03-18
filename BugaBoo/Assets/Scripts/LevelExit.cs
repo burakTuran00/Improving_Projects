ecb84f67a05a2f81580890c727683194056dd9ab
burakTuran00 <burakturan7410@gmail.com>
2023-03-18 18:05:21 +0300

    BugaBoo

---
+++ BugaBoo/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    public float levelLoadDelay = 1.0f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            StartCoroutine(LoadNextLevel());
+        }
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            currentSceneIndex = 0;
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene (nextSceneIndex);
+    }
+}
