d43dc9b680318d900c397c7237d3a00849b7174e
burakTuran00 <burakturan7410@gmail.com>
2023-03-18 22:35:12 +0300

    BugaBoo

--- BugaBoo/Assets/Scripts/PickUpCoin.cs
+++ BugaBoo/Assets/Scripts/PickUpCoin.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class PickUpCoin : MonoBehaviour
 {
+    public AudioClip sfx;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.transform.CompareTag("Player"))
         {
+            AudioSource.PlayClipAtPoint(sfx, Camera.main.transform.position);
             Destroy (gameObject);
         }
     }
