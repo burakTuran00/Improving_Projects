bde4469b1b2c7c97a6ae2953a0a43c51c6562bde
burakTuran00 <burakturan7410@gmail.com>
2023-03-27 14:13:11 +0300

    BugaBoo Chapter Two

--- BugaBoo Chapter Two/Assets/Scripts/EnemyMovement.cs
+++ BugaBoo Chapter Two/Assets/Scripts/EnemyMovement.cs
@@ -2,30 +2,32 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public float enemyMoveSpeed = -1.0f;
+    public float enemyMoveSpeed = 1.0f;
 
     private Rigidbody2D rb;
 
-    private Transform Direction;
+    public float directionX = 1.0f;
 
     private void Awake()
     {
+        transform.Rotate(new Vector3(0f, 180f, 0f));
         rb = GetComponent<Rigidbody2D>();
+        directionX = transform.localScale.x;
     }
 
     private void Update()
     {
-        rb.velocity = new Vector2(enemyMoveSpeed, 0.0f);
+        rb.velocity = new Vector2(directionX * enemyMoveSpeed, 0f);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D other)
     {
-        enemyMoveSpeed = -enemyMoveSpeed;
         FlipEnemy();
     }
 
     private void FlipEnemy()
     {
         transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), 1.0f);
+        directionX = transform.localScale.x;
     }
 }
