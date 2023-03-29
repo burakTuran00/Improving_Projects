cde7b396b0df4adb922c96db3121b0090c918b34
burakTuran00 <burakturan7410@gmail.com>
2023-03-29 19:12:46 +0300

    BugaBoo V2

---
+++ BugaBooV2/Assets/Scripts/GetCoin.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GetCoin : MonoBehaviour
+{
+    public AudioClip coinSound;
+
+    private bool wasPicked = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !wasPicked)
+        {
+            wasPicked = true;
+            AudioSource
+                .PlayClipAtPoint(coinSound, Camera.main.transform.position);
+            Destroy (gameObject);
+        }
+    }
+}
