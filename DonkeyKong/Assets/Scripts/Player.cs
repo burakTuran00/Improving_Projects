dc64d162fd58b2a58c5e8ea0b08918900b0bc7da
burakTuran00 <burakturan7410@gmail.com>
2023-04-15 15:50:53 +0300

    DonkeyKong

--- DonkeyKong/Assets/Scripts/Player.cs
+++ DonkeyKong/Assets/Scripts/Player.cs
@@ -4,19 +4,53 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
 
+    private new Collider2D collider;
+
+    private Collider2D[] results = new Collider2D[4];
+
     private Vector2 direction;
 
     public float moveSpeed = 1.0f;
 
     public float jumpStrength = 1.0f;
 
+    private bool grounded;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    private void CheckCollision()
+    {
+        grounded = false;
+
+        Vector2 size = collider.bounds.size;
+        size.y += 0.1f;
+        size.x /= 2.0f;
+
+        int amount =
+            Physics2D.OverlapBoxNonAlloc(transform.position, size, 0, results);
+
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject hit = results[i].gameObject;
+
+            if (hit.layer == LayerMask.NameToLayer("Ground"))
+            {
+                grounded =
+                    hit.transform.position.y < (transform.position.y - 0.5f);
+
+                Physics2D.IgnoreCollision(collider, results[i], !grounded);
+            }
+        }
     }
 
     private void Update()
     {
+        CheckCollision();
+
         Jumping();
         PlayerMovement();
         PlayerFlip();
@@ -25,7 +59,11 @@ public class Player : MonoBehaviour
     private void PlayerMovement()
     {
         direction.x = Input.GetAxis("Horizontal") * moveSpeed;
-        direction.y = Mathf.Max(direction.y, -1.0f);
+
+        if (grounded)
+        {
+            direction.y = Mathf.Max(direction.y, -1.0f);
+        }
 
         rb.MovePosition(rb.position + direction * Time.fixedDeltaTime);
     }
@@ -44,7 +82,7 @@ public class Player : MonoBehaviour
 
     private void Jumping()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && grounded)
         {
             direction = Vector2.up * jumpStrength;
         }
