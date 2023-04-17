809de1ea9dd4d2c0a7b4c39655941480b487f499
burakTuran00 <burakturan7410@gmail.com>
2023-04-17 15:15:47 +0300

    BrickBreaker

--- BrickBreaker/Assets/Scripts/Brick.cs
+++ BrickBreaker/Assets/Scripts/Brick.cs
@@ -2,11 +2,53 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer {get;private set}
+    public SpriteRenderer spriteRenderer { get; private set; }
+
+    public Sprite[] state;
+
     public int health { get; private set; }
 
-    private void Awake() {
-        
+    public bool unbreakable;
+
+    private void Awake()
+    {
         spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        if (!unbreakable)
+        {
+            health = state.Length;
+            spriteRenderer.sprite = state[health - 1];
+        }
+    }
+
+    private void Hit()
+    {
+        if (unbreakable)
+        {
+            return;
+        }
+
+        health--;
+
+        if (health <= 0)
+        {
+            this.gameObject.SetActive(false);
+            Destroy (gameObject);
+        }
+        else
+        {
+            spriteRenderer.sprite = state[health - 1];
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            Hit();
+        }
     }
 }
