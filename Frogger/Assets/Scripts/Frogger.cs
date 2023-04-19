5c0f5382f45d4db01fc98c4762d46da48805c224
burakTuran00 <burakturan7410@gmail.com>
2023-04-19 21:12:40 +0300

    Frogger

--- Frogger/Assets/Scripts/Frogger.cs
+++ Frogger/Assets/Scripts/Frogger.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
-
+using System.Collections;
 public class Frogger : MonoBehaviour
 {
+
+    private SpriteRenderer spriteRenderer;
+    public Sprite idleSprite;
+    public Sprite leapSprite;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
@@ -28,11 +38,33 @@ public class Frogger : MonoBehaviour
 
     private void Move(Vector3 direction)
     {
-        transform.position += direction;
+        Vector3 destinaiton = transform.position+ direction;
+        StartCoroutine(Leap(destinaiton));
     }
 
     private void RotateFrog(float zRot)
     {
         transform.rotation = Quaternion.Euler(0f, 0f, zRot);
+    }
+
+    IEnumerator Leap(Vector3 destination)
+    {
+        Vector3 startPosition = transform.position;
+
+        float elapsed = 0.0f;
+        float duration = 0.125f;
+
+        spriteRenderer.sprite = leapSprite;
+
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+            transform.position = Vector3.Lerp(startPosition, destination, t);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }    
+
+        transform.position = destination;
+        spriteRenderer.sprite = idleSprite;
     }
 }
