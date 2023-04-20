35f1ef1147ecff18716dc41d86a8a9e7c7bea6db
burakTuran00 <burakturan7410@gmail.com>
2023-04-20 11:24:36 +0300

    Frogger

--- Frogger/Assets/Scripts/Frogger.cs
+++ Frogger/Assets/Scripts/Frogger.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System.Collections;
 public class Frogger : MonoBehaviour
 {
-
     private SpriteRenderer spriteRenderer;
     public Sprite idleSprite;
     public Sprite leapSprite;
--- Frogger/Assets/Scripts/MoveCycle.cs
+++ Frogger/Assets/Scripts/MoveCycle.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveCycle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 direction = Vector2.right;
+
+    public float speed = 1f;
+
+    public int size = 1;
+
+    private Vector2 leftEdge;
+
+    private Vector2 rightEdge;
+
+    private void Start()
     {
-        
+        leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (direction.x > 0 && (transform.position.x - size) > rightEdge.x)
+        {
+            Vector3 position = transform.position;
+            position.x = leftEdge.x - size;
+            transform.position = position;
+        }
+        else if (direction.x < 0 && (transform.position.x + size) < leftEdge.x)
+        {
+            Vector3 position = transform.position;
+            position.x = rightEdge.x + size;
+            transform.position = position;
+        }
+        else
+        {
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
     }
 }
