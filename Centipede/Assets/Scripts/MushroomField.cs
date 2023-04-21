5e985ee20dbc7c49c994845f7973ff03529e146c
burakTuran00 <burakturan7410@gmail.com>
2023-04-21 18:56:44 +0300

    Centipede

--- Centipede/Assets/Scripts/MushroomField.cs
+++ Centipede/Assets/Scripts/MushroomField.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MushroomField : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private BoxCollider2D area;
+
+    public Mushroom prefab;
+
+    public int amount = 50;
+
+    private void Awake()
     {
-        
+        area = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Generate();
+    }
+
+    private void Generate()
+    {
+        Bounds bounds = area.bounds;
+
+        for (int i = 0; i < amount; i++)
+        {
+            Vector2 position = Vector2.zero;
+
+            position.x = Mathf.Round(Random.Range(bounds.min.x, bounds.max.x));
+            position.y = Mathf.Round(Random.Range(bounds.min.y, bounds.max.y));
+
+            Instantiate(prefab, position, Quaternion.identity, this.transform);
+        }
     }
 }
