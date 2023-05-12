e8b5b411e8607e4578caa00e7d16d8fce1320b05
burakTuran00 <burakturan7410@gmail.com>
2023-05-12 19:30:41 +0300

    Mario

--- Mario/Assets/Scripts/PlayerMovement.cs
+++ Mario/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb;
+
+    private Vector2 velocity;
+
+    private float inputAxis;
+
+    [Range(1, 14)]
+    public float moveSpeed = 8.0f;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        HorizontalMovement();
+    }
+
+    private void FixedUpdate() 
+    {
+        Vector2 position = rb.position;
+        position += velocity * Time.fixedDeltaTime;
+
+        rb.MovePosition(position);    
+    }
+
+    private void HorizontalMovement()
+    {
+        inputAxis = Input.GetAxis("Horizontal");
+        velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, Time.deltaTime * moveSpeed);
     }
 }
