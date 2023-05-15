66ef31a8d88f4249d961b9443054878f10642bca
burakTuran00 <burakturan7410@gmail.com>
2023-05-15 21:44:47 +0300

    Mario

--- Mario/Assets/Scripts/GameManager.cs
+++ Mario/Assets/Scripts/GameManager.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-   
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            DestroyImmediate (gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad (gameObject);
+        }
+    }
 }
