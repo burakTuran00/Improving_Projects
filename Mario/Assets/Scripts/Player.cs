06c3cf670e9f34c3a45bd9dba1122badeb0d4bfd
burakTuran00 <burakturan7410@gmail.com>
2023-05-17 16:49:07 +0300

    Mario

--- Mario/Assets/Scripts/Player.cs
+++ Mario/Assets/Scripts/Player.cs
@@ -2,7 +2,41 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public PlayerSpriteRenderer smallRenderer;
+
+    public PlayerSpriteRenderer bigRenderer;
+
+    private DeathAnimation deathAnimation;
+
+    public bool big => bigRenderer.enabled;
+
+    public bool small => smallRenderer.enabled;
+
+    public bool death => deathAnimation.enabled;
+
+    private void Awake()
+    {
+        deathAnimation = GetComponent<DeathAnimation>();
+    }
+
     public void Hit()
+    {
+        if (big)
+        {
+            Shrink();
+        }
+        else
+        {
+            Death();
+        }
+    }
+
+    private void Shrink()
+    {
+        //todo
+    }
+
+    private void Death()
     {
         
     }
