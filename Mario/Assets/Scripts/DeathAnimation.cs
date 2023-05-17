adbe8bb786adb4a61d9e6cfc5b02ef0747a7154a
burakTuran00 <burakturan7410@gmail.com>
2023-05-17 16:15:22 +0300

    Mario

--- Mario/Assets/Scripts/DeathAnimation.cs
+++ Mario/Assets/Scripts/DeathAnimation.cs
@@ -6,5 +6,34 @@ public class DeathAnimation : MonoBehaviour
 
     public SpriteRenderer spriteRenderer;
 
-    
+    private void Reset()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        UpdateSprite();
+        DisablePhysics();
+        Animate();
+    }
+
+    private void UpdateSprite()
+    {
+        spriteRenderer.enabled = true;
+        spriteRenderer.sortingOrder = 10;
+
+        if (deathSprite != null)
+        {
+            spriteRenderer.sprite = deathSprite;
+        }
+    }
+
+    private void DisablePhysics()
+    {
+    }
+
+    private void Animate()
+    {
+    }
 }
