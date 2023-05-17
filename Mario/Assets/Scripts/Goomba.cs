03c5387fd2efaa694d2b3afb4a1c4f7c2f7fa605
burakTuran00 <burakturan7410@gmail.com>
2023-05-17 15:56:36 +0300

    Mario

--- Mario/Assets/Scripts/Goomba.cs
+++ Mario/Assets/Scripts/Goomba.cs
@@ -2,4 +2,13 @@ using UnityEngine;
 
 public class Goomba : MonoBehaviour
 {
+    public Sprite flatSprite;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+        }
+    }
 }
