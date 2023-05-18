3b49e5897224b8ed76c6de0c0e3d30486588b709
burakTuran00 <burakturan7410@gmail.com>
2023-05-18 16:27:38 +0300

    Mario

---
+++ Mario/Assets/Scripts/Koopa.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Koopa : MonoBehaviour
+{
+    public Sprite shellSprite;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Player player = other.gameObject.GetComponent<Player>();
+
+            if (other.transform.DotTest(transform, Vector2.down))
+            {
+                EnterShell();
+            }
+            else
+            {
+                player.Hit();
+            }
+        }
+    }
+
+    private void EnterShell()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<EntityMovement>().enabled = false;
+        GetComponent<AnimatedSprite>().enabled = false;
+        GetComponent<SpriteRenderer>().sprite = shellSprite;
+    }
+}
