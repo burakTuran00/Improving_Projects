5f783359482ff4056a387613b2930605dbc48502
burakTuran00 <burakturan7410@gmail.com>
2023-05-18 16:41:59 +0300

    Mario

--- Mario/Assets/Scripts/Koopa.cs
+++ Mario/Assets/Scripts/Koopa.cs
@@ -4,9 +4,11 @@ public class Koopa : MonoBehaviour
 {
     public Sprite shellSprite;
 
+    public float shellSpeed = 12.0f;
+
     private bool shelled;
 
-    private bool shellMoving;
+    private bool pushed;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -25,6 +27,23 @@ public class Koopa : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && shelled)
+        {
+            if (!pushed)
+            {
+                Vector2 direction =new Vector2(transform.position.x - other.transform.position.x, 0f);
+                PushCell(direction);
+            }
+            else
+            {
+                Player player = other.GetComponent<Player>();
+                player.Hit();
+            }
+        }
+    }
+
     private void EnterShell()
     {
         shelled = true;
@@ -32,5 +51,17 @@ public class Koopa : MonoBehaviour
         GetComponent<EntityMovement>().enabled = false;
         GetComponent<AnimatedSprite>().enabled = false;
         GetComponent<SpriteRenderer>().sprite = shellSprite;
+    }
+
+    private void PushCell(Vector2 direction)
+    {
+        pushed = true;
+
+        GetComponent<Rigidbody2D>().isKinematic = true;
+        
+        EntityMovement entityMovement = GetComponent<EntityMovement>();
+        entityMovement.direction = direction.normalized;
+        entityMovement.speed = shellSpeed;
+        entityMovement.enabled = true;
     }
 }
