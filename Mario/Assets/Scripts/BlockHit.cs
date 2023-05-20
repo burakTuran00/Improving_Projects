171c7ae1c43765263189013f4e8a814e648f2929
burakTuran00 <burakturan7410@gmail.com>
2023-05-20 14:00:39 +0300

    Mario

--- Mario/Assets/Scripts/BlockHit.cs
+++ Mario/Assets/Scripts/BlockHit.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class BlockHit : MonoBehaviour
 {
     public int maxHits = -1;
+
+    public Sprite emptyBlock;
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -16,6 +19,13 @@ public class BlockHit : MonoBehaviour
 
     private void Hit()
     {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
 
+        maxHits--;
+
+        if (maxHits == 0)
+        {
+            spriteRenderer.sprite = emptyBlock;
+        }
     }
 }
