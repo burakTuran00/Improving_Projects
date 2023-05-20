f13d23b777b72f4baef418e9fb89e077ca037333
burakTuran00 <burakturan7410@gmail.com>
2023-05-20 15:31:37 +0300

    Mario

---
+++ Mario/Assets/Scripts/BlockItem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+
+public class BlockItem : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(Animate());
+    }
+
+    private IEnumerator Animate()
+    {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        CircleCollider2D physicsCollider = GetComponent<CircleCollider2D>();
+        BoxCollider2D triggerCollider = GetComponent<BoxCollider2D>();
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        rigidbody.isKinematic = true;
+        physicsCollider.enabled = false;
+        triggerCollider.enabled = false;
+        spriteRenderer.enabled = false;
+    }
+}
