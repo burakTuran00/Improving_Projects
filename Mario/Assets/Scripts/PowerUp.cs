8635dff560beae8ece254f29e27faa1d567e9393
burakTuran00 <burakturan7410@gmail.com>
2023-05-21 14:07:39 +0300

    Mario

---
+++ Mario/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public enum Type
+    {
+        Coin,
+        ExtraLife,
+        MagicMushroom,
+        Starpower
+    }
+
+    public Type type;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if ()
+        {
+            
+        }
+    }
+}
