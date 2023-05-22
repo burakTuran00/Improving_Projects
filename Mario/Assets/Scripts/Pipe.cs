86d76236729df571197e1333477c0333a5fe14d4
burakTuran00 <burakturan7410@gmail.com>
2023-05-22 11:30:06 +0300

    Mario

---
+++ Mario/Assets/Scripts/Pipe.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    public Transform connection;
+
+    public KeyCode enterKeyCode = KeyCode.S;
+
+    public Vector3 enterDirection = Vector3.down;
+
+    public Vector3 exitDirection = Vector3.zero;
+
+    private void OnTriggerStay(Collider2D other)
+    {
+        if (other.CompareTag("Player") && connection != null)
+        {
+            if (Input.GetKeyDown(enterKeyCode))
+            {
+                StartCoroutine(Enter(other.transform));
+            }
+        }
+    }
+
+    private IEnumerator Enter(Transform player)
+    {
+        player.GetComponent<PlayerMovement>().enabled = false;
+
+        Vector3 enteredPosition = transform.position + enterDirection;
+        Vector3 enteredScale = Vector3.one * 0.5f;
+
+        yield return Move(player, enteredPosition, enteredScale);
+    }
+
+    private IEnumerator
+    Move(Transform player, Vector3 endPosition, Vector3 endScale)
+    {
+        float elapsed = 0f;
+        float duration = 1f;
+
+        Vector3 startPosition = player.position;
+        Vector3 startScale = player.localScale;
+
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+
+            player.position = Vector3.Lerp(startPosition, endPosition, t);
+            player.localScale = Vector3.Lerp(startScale, endScale, t);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        player.position = endPosition;
+        player.localScale = endScale;
+    }
+}
