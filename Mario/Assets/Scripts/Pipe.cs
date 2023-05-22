4e4b8f2eb19939bc944df5d382605fe564b04903
burakTuran00 <burakturan7410@gmail.com>
2023-05-22 11:41:48 +0300

    Mario

--- Mario/Assets/Scripts/Pipe.cs
+++ Mario/Assets/Scripts/Pipe.cs
@@ -30,6 +30,20 @@ public class Pipe : MonoBehaviour
         Vector3 enteredScale = Vector3.one * 0.5f;
 
         yield return Move(player, enteredPosition, enteredScale);
+        yield return new WaitForSeconds(1f);
+
+        if (exitDirection != Vector3.zero)
+        {
+            player.position = connection.position - exitDirection;
+            yield return Move(player, connection.position + exitDirection, Vector3.one);
+        }
+        else
+        {
+            player.position = connection.position;
+            player.localScale = Vector3.one;
+        }
+
+        player.GetComponent<PlayerMovement>().enabled = true;
     }
 
     private IEnumerator
