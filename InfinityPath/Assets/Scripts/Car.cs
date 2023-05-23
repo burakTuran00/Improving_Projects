3e6839f3d2f2c863d36b61ac7c011b33d5f92c6b
burakTuran00 <burakturan7410@gmail.com>
2023-05-23 12:36:06 +0300

    InfinityPath

--- InfinityPath/Assets/Scripts/Car.cs
+++ InfinityPath/Assets/Scripts/Car.cs
@@ -13,27 +13,9 @@ public class Car : MonoBehaviour
 
     private void Move()
     {
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            direction = Vector3.left;
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            direction = Vector3.right;
-        }
-        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            direction = Vector3.up;
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            direction = Vector3.down;
-        }
-        else
-        {
-            direction = Vector3.zero;
-        }
+        direction.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        direction.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
-        transform.position += direction * speed * Time.deltaTime;
+        transform.position += direction;
     }
 }
