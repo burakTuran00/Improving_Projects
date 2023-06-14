07f69778ee7c1d255ee82f20fde0004ca0622021
burakTuran00 <burakturan7410@gmail.com>
2023-06-14 20:09:29 +0300

    Runner

--- Runner/Assets/Scripts/DetectCollision.cs
+++ Runner/Assets/Scripts/DetectCollision.cs
@@ -10,7 +10,7 @@ public class DetectCollision : MonoBehaviour
         {
             AddCoin();
 
-            other.gameObject.SetActive(false);
+            Destroy(other.gameObject);
         }
     }
 
