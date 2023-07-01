c5ad90ce99e1d54b067330c02a1cf8e50c0cc01a
burakTuran00 <burakturan7410@gmail.com>
2023-07-01 13:13:11 +0300

    SpaceShooter

--- SpaceShooter/Assets/Scripts/Meteor.cs
+++ SpaceShooter/Assets/Scripts/Meteor.cs
@@ -6,18 +6,29 @@ public class Meteor : MonoBehaviour
 
     public Sprite[] meteors;
 
-    public float meteorSpeed = 1.0f;
+    public float meteorSpeed => Random.Range(3, 12);
 
-    public int helath => Random.Range(1, 5);
+    public int health => Random.Range(1, 5);
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
+    private void Start()
+    {
+        GetIndexMeteor();
+    }
+
     private void Update()
     {
         MeteorMovement();
+    }
+
+    private void GetIndexMeteor()
+    {
+        int index = Random.Range(0, meteors.Length);
+        spriteRenderer.sprite = meteors[index];
     }
 
     private void MeteorMovement()
