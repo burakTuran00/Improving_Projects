9e504b6c41beae8007ba5ef2e4ac685d233c302e
burakTuran00 <burakturan7410@gmail.com>
2023-07-03 17:48:41 +0300

    InvadersFromSpace

--- InvadersFromSpace/Assets/Scripts/Invaders.cs
+++ InvadersFromSpace/Assets/Scripts/Invaders.cs
@@ -1,18 +1,44 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Invaders : MonoBehaviour
 {
     public Invader[] prefabs;
 
+    public Laser laser;
+
     public int rows = 5;
 
-    public int columms = 8;
+    public int columms = 11;
+
+    public AnimationCurve speed;
+
+    public float missileAttackRate = 1.0f;
+
+    public int amountKilled { get; private set; }
+
+    public int amountAlive => totalInvaders - amountKilled;
+
+    public int totalInvaders => rows * columms;
+
+    public float percentKilled => (float) amountKilled / (float) totalInvaders;
 
     private Vector3 direction = Vector2.right;
 
     public void Awake()
     {
+        CreatingInvaders();
+        laser.direciton = Vector3.down;
     }
+
+     private void Start()
+    {
+        InvokeRepeating(nameof(MissileAttack),missileAttackRate, missileAttackRate);
+    }
+    private void Update() 
+    {
+        MovingInvaders();    
+    }
+
 
     private void CreatingInvaders()
     {
@@ -35,4 +61,70 @@ public class Invaders : MonoBehaviour
             }
         }
     }
+
+    private void MovingInvaders()
+    {
+        transform.position +=
+            direction * speed.Evaluate(percentKilled) * Time.deltaTime;
+
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+        foreach (Transform invader in this.transform)
+        {
+            if (!invader.gameObject.activeInHierarchy)
+            {
+                continue;
+            }
+
+            if (direction == Vector3.right && invader.position.x >= (rightEdge.x - 1)
+            )
+            {
+                AdvancedRow();
+            }
+            else if (direction == Vector3.left && invader.position.x <= (leftEdge.x + 1)
+            )
+            {
+                AdvancedRow();
+            }
+        }
+    }
+
+     private void AdvancedRow()
+    {
+        direction.x *= -1;
+
+        Vector3 position = this.transform.position;
+        position.y -= 1;
+
+        this.transform.position = position;
+    }
+
+      public void InvaderKilled()
+    {
+        amountKilled++;
+
+        if (amountKilled >= totalInvaders)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private void MissileAttack()
+    {
+        foreach (Transform invader in this.transform)
+        {
+            if (!invader.gameObject.activeInHierarchy)
+            {
+                continue;
+            }
+
+            if (Random.value < (1.0f / (float) amountAlive))
+            {
+                Instantiate(laser,invader.position,Quaternion.identity);
+                break;
+            }
+        }
+    }
+
 }
