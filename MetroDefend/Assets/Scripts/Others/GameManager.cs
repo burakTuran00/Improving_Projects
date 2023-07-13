5dc976763a6fd2d0cea14db210651660f697662c
burakTuran00 <burakturan7410@gmail.com>
2023-07-13 14:50:48 +0300

    MetroDefend

--- MetroDefend/Assets/Scripts/Others/GameManager.cs
+++ MetroDefend/Assets/Scripts/Others/GameManager.cs
@@ -1,12 +1,12 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
     public float delay = 1.0f;
 
-    private void Awake()
+    private void Detect()
     {
         int numberGameManager = FindObjectsOfType<GameManager>().Length;
 
@@ -30,6 +30,12 @@ public class GameManager : MonoBehaviour
     {
         int currentIndex = SceneManager.GetActiveScene().buildIndex;
         int nextIndex = currentIndex + 1;
+
+        if (nextIndex >= SceneManager.sceneCountInBuildSettings)
+        {
+            nextIndex = 0;
+        }
+
         StartCoroutine(SceneLoader(nextIndex));
     }
 
