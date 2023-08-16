7f0823060368e28a25402dd0b677b21f1b22c823
burakTuran00 <burakturan7410@gmail.com>
2023-08-16 15:03:35 +0300

    PixelWar2D

--- PixelWar2D/Assets/Scripts/Bullet.cs
+++ PixelWar2D/Assets/Scripts/Bullet.cs
@@ -28,11 +28,11 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.transform.CompareTag("Ground"))
-        {
-            Destroy(other.gameObject);
-        }
+        Destroy (gameObject);
+    }
 
+    private void OnCollisionEnter2D(Collision2D other)
+    {
         Destroy (gameObject);
     }
 }
