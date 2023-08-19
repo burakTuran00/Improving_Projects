823352302da65fa7ec6b32723b8a768c11015a7c
burakTuran00 <burakturan7410@gmail.com>
2023-08-19 15:00:27 +0300

    PixelWar2D

--- PixelWar2D/Assets/Scripts/PlayerHealth.cs
+++ PixelWar2D/Assets/Scripts/PlayerHealth.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
-
+using TMPro;
 public class PlayerHealth : MonoBehaviour
 {
     private Animator animator;
 
     public int health = 100;
 
+    public TextMeshProUGUI healthText;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        healthText.text = "x" + health.ToString();
     }
 
     public void TakeDamage(int amount)
@@ -23,5 +26,7 @@ public class PlayerHealth : MonoBehaviour
         {
             animator.SetTrigger("isHurting");
         }
+
+        healthText.text = "x" + health.ToString();
     }
 }
