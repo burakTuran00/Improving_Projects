c34c8516cf0f9261dcd443a65dbe28b9949ba648
burakTuran00 <burakturan7410@gmail.com>
2023-08-25 21:54:02 +0300

    PixelWar2D

--- PixelWar2D/Assets/Scripts/GameManager.cs
+++ PixelWar2D/Assets/Scripts/GameManager.cs
@@ -3,6 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private void Detect()
+    {
+        int numberGameManager = FindObjectsOfType<GameManager>().Length;
+
+        if (numberGameManager > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
     public void RestartLevel()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
@@ -13,6 +27,12 @@ public class GameManager : MonoBehaviour
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+        {
+            currentSceneIndex = 0;
+            nextSceneIndex = 0;
+        }
 
         SceneManager.LoadScene (nextSceneIndex);
     }
